5085d6362a3d065136c998218a3ff82f6a855939
afdhal <afdhalnejib@gmail.com>
2024-06-15 11:47:40 +0100

    addd update

--- src/screens/account/account.js
+++ src/screens/account/account.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
+import MapLibreGL from '@maplibre/maplibre-react-native';
+import styles from './account.styles';
+// Quirky step required on Android. See Android installation notes.
+MapLibreGL.setAccessToken(null);
+
+const MAPTILER_API_KEY = "EEGDPU7G3Z4AgIsMeeGD";
+const styleUrl = `https://api.maptiler.com/maps/streets-v2/style.json?key=${MAPTILER_API_KEY}`;
+
 const Account = () => {
   return (
-    <SafeAreaView>
-      <Text>Account</Text>
-    </SafeAreaView>
+    <View style={styles.page}>
+       <MapLibreGL.MapView
+          style={styles.map}
+          styleURL={styleUrl}
+          logoEnabled={false}
+          attributionPosition={{bottom: 8, right: 8}}>
+          <MapLibreGL.Camera
+             defaultSettings={{centerCoordinate: [8.775656, 34.431141], zoomLevel: 8}}
+             />
+             <MapLibreGL.PointAnnotation
+            id="userLocation"
+            coordinate={[8.775656, 34.431141]}
+            title="Your location"
+          />
+
+<MapLibreGL.MarkerView
+            coordinate={[8.775656, 34.431141]}
+            children={
+              <Text
+              style={{fontSize:20,fontWeight:"bold"}}
+              >
+
+                Station lavage
+                </Text>
+            }
+            anchor={{x: 0, y: 0.5}}
+          />
+        </MapLibreGL.MapView>
+      </View>
   );
 };
 
---
+++ src/screens/account/account.styles.js
@@ -0,0 +1,23 @@
+import {View, StyleSheet, Platform, StatusBar} from 'react-native';
+import Colors from '../../utils/colors';
+import {ColorSpace} from 'react-native-reanimated';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+
+  page: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  map: {
+    flex: 1,
+    alignSelf: 'stretch',
+  },
+});
+
+export default styles;
